aaee6667306176615b4bdfd6916702eaed8518b7
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2024-01-30 22:11:45 +0100

    Adding new test for same not same and true and not true

--- src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
+++ src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
@@ -4,9 +4,49 @@ import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+//@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+//@DisplayNameGeneration(DisplayNameGenerator.Simple.class)
+//@DisplayNameGeneration(DisplayNameGenerator.IndicativeSentences.class)
 class DemoUtilsTest {
 
     DemoUtils demoUtils;
+
+    @Test
+    @DisplayName("Equals and not equals")
+    void testEqualsAndNotEquals() {
+        assertEquals(6, demoUtils.add(2,4),"2 + 4 must be 6");
+        assertNotEquals(6, demoUtils.add(1,9),"1 + 9 must not be 6");
+    }
+
+    @Test
+    @DisplayName("Null and not null")
+    void testNullAndNotNull(){
+        String str1 = null;
+        String str2 = "null";
+
+        assertNull(demoUtils.checkNull(str1),"Object should be null");
+        assertNotNull(demoUtils.checkNull(str2),"Object should not be null");
+    }
+
+    @Test
+    @DisplayName("Same and not same")
+    void testSameAndNotSame(){
+        String str = "luv2code";
+
+        assertSame(demoUtils.getAcademy(),demoUtils.getAcademyDuplicate(),"Objects should refer to same object");
+        assertNotSame(str,demoUtils.getAcademy(),"Objects should not refer to same object");
+    }
+
+    @Test
+    @DisplayName("True and false")
+    void testTrueFalse(){
+
+        int gradeOne=50;
+        int gradeTwo=5;
+
+        assertTrue(demoUtils.isGreater(gradeOne, gradeTwo),"This should return true");
+        assertFalse(demoUtils.isGreater(gradeTwo, gradeOne),"This should return false");
+    }
 
     @AfterEach
     void tearDownAfterEach(){
@@ -29,22 +69,5 @@ class DemoUtilsTest {
     @BeforeAll
     static void setUpBeforeEachClass(){
         System.out.println("@BeforeAll executes only once before all test methods");
-    }
-
-    @Test
-    void testEqualsAndNotEquals() {
-        System.out.println("Running test: testEqualsAndNotEquals");
-        assertEquals(6, demoUtils.add(2,4),"2 + 4 must be 6");
-        assertNotEquals(6, demoUtils.add(1,9),"1 + 9 must not be 6");
-    }
-
-    @Test
-    void testNullAndNotNull(){
-        String str1 = null;
-        String str2 = "null";
-
-        System.out.println("Running test: testNullAndNotNull");
-        assertNull(demoUtils.checkNull(str1),"Object should be null");
-        assertNotNull(demoUtils.checkNull(str2),"Object should not be null");
     }
 }
