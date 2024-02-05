1119993b85d0a9830f3aee49eccab0fec4e4612f
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2024-02-05 22:57:04 +0100

    Adding tests for jre range

--- src/test/java/com/luv2code/junitdemo/ConditionalTest.java
+++ src/test/java/com/luv2code/junitdemo/ConditionalTest.java
@@ -2,8 +2,7 @@ package com.luv2code.junitdemo;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.EnabledOnOs;
-import org.junit.jupiter.api.condition.OS;
+import org.junit.jupiter.api.condition.*;
 
 class ConditionalTest {
     @Test
@@ -27,6 +26,30 @@ class ConditionalTest {
     @Test
     @EnabledOnOs({OS.LINUX,OS.WINDOWS})
     void testForLinuxAndWindowsOnly(){
+
+    }
+
+    @Test
+    @EnabledOnJre(JRE.JAVA_13)
+    void testForJava13Only(){
+
+    }
+
+    @Test
+    @EnabledOnJre(JRE.JAVA_17)
+    void testForJava17Only(){
+
+    }
+
+    @Test
+    @EnabledForJreRange(min=JRE.JAVA_17,max=JRE.JAVA_21)
+    void testForJavaRange(){
+
+    }
+
+    @Test
+    @EnabledForJreRange(min=JRE.JAVA_13)
+    void testForJavaMin(){
 
     }
 }
