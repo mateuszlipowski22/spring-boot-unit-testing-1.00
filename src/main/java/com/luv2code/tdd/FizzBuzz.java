f8c4b2beafab54fc286ec2684b466ac4250e1031
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2024-02-06 22:16:28 +0100

    Refactoring

--- src/main/java/com/luv2code/tdd/FizzBuzz.java
+++ src/main/java/com/luv2code/tdd/FizzBuzz.java
@@ -2,21 +2,29 @@ package com.luv2code.tdd;
 
 public class FizzBuzz {
 
-    public static String compute(int i) {
+//    public static String compute(int i) {
+//        if(i%3==0 && i%5==0){
+//            return "FizzBuzz";
+//        }
+//        if(i%3==0){
+//            return "Fizz";
+//        }
+//        if(i%5==0){
+//            return "Buzz";
+//        }
+//        return String.valueOf(i);
+//    }
 
-        if(i%3==0 && i%5==0){
-            return "FizzBuzz";
-        }
+    public static String compute(int i) {
+        StringBuilder result = new StringBuilder();
 
         if(i%3==0){
-            return "Fizz";
+            result.append("Fizz");
         }
-
         if(i%5==0){
-            return "Buzz";
+            result.append("Buzz");
         }
-
-        return String.valueOf(i);
+        return result.isEmpty() ? String.valueOf(i) : result.toString();
     }
 
 }
